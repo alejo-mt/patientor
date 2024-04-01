fc9c1596aa9d4d93c6015ac34b4f112e424fe974
zubstack <mt.calderon.alejandro@gmail.com>
2024-04-01 09:24:09 -0500

    use parser functions

--- server/src/utils/toNewPatient.ts
+++ server/src/utils/toNewPatient.ts
@@ -1,11 +1,9 @@
 import { Gender, NewPatient } from "../types";
 
 const toNewPatient = (payload: unknown): NewPatient => {
-    // Check if it exists and if it is an object
     if (!payload || !(payload instanceof Object))
         throw Error("Missing or bad payload");
 
-    // Check if it has the expected propeties
     if (
         !("name" in payload) ||
         !("dateOfBirth" in payload) ||
@@ -13,33 +11,42 @@ const toNewPatient = (payload: unknown): NewPatient => {
         !("gender" in payload) ||
         !("occupation" in payload)
     )
-        throw Error("Missing payload properties");
-    const { name, dateOfBirth, ssn, gender, occupation } = payload;
-
-    // Check if the expected properties have the correct type
-    if (!isString(name)) throw Error("`name` must to be an string");
-    if (!isString(dateOfBirth))
-        throw Error("`dateOfBirth` must to be an string");
-    if (!isString(ssn)) throw Error("`ssn` must to be an string");
-    if (!isString(gender)) throw Error("`gender` must to be an string");
-    if (!isString(occupation)) throw Error("`occupation` must to be an string");
-
-    // Check if some specific properties have the correct value
-    if (!isGender(gender)) throw Error("`gender` must to be valid");
+        throw Error("Incorrect data, some fields are missing");
 
     const newPatient: NewPatient = {
-        name: name,
-        dateOfBirth: dateOfBirth,
-        ssn: ssn,
-        gender: gender,
-        occupation: occupation,
+        name: parseString(payload.name),
+        dateOfBirth: parseDate(payload.dateOfBirth),
+        ssn: parseString(payload.ssn),
+        gender: parseGender(payload.gender),
+        occupation: parseString(payload.occupation),
     };
 
     return newPatient;
 };
 
-const isString = (value: unknown): value is string => {
-    return typeof value === "string";
+const parseString = (value: unknown): string => {
+    if (!isString(value)) throw Error(`Incorrect or missing value: ${value}`);
+    return value;
+};
+
+const parseGender = (value: unknown) => {
+    if (!isString(value) || !isGender(value))
+        throw Error(`Incorrect or missing gender: ${value}`);
+    return value;
+};
+
+const parseDate = (value: unknown) => {
+    if (!isString(value) || !isDate(value))
+        throw Error(`Incorrect or missing date: ${value}`);
+    return value;
+};
+
+const isString = (text: unknown): text is string => {
+    return typeof text === "string";
+};
+
+const isDate = (date: string): boolean => {
+    return Boolean(Date.parse(date));
 };
 
 const isGender = (value: string): value is Gender => {
