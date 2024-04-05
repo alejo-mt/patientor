0298e6caffbfc389f15c1631bd9e8e24309112bb
zubstack <mt.calderon.alejandro@gmail.com>
2024-04-05 10:06:38 -0500

    /patient - entries

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,4 +1,41 @@
-export interface Entry {}
+interface BaseEntry {
+    id: string;
+    date: string;
+    description: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+type Entry = HealthCheckEntry | OccupationalHealthcareEntry | HospitalEntry;
+
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: {
+        startDate: string;
+        endDate: string;
+    };
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge?: {
+        date: string;
+        criteria: string;
+    };
+}
+
+enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3,
+}
 
 export interface Diagnosis {
     code: string;
